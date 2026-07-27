7fd1b7ba40cedde2b0169817ba80ee138daf05ef
buildwithanas <anasamoo0704@gmail.com>
2026-07-27 20:45:35 +0100

    Add Day_2

---
+++ Day_2/assignment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+  //Introduction
+  printf("Welcome to C programming\n");
+  printf("My name is Anas\n");
+  printf("I'm learning to become a Software Engineer\n");
+
+  return 0;
+}
---
+++ Day_2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+  //This is my second C program! :)
+  
+  printf("My name is Anas\n");
+  printf("I'm learning C programming from scratch");
+
+  return 0;
+}
