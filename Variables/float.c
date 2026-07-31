3f098a7b4bbec1ed6a573a99fa05e4392d09a21f
buildwithanas <anasamoo0704@gmail.com>
2026-07-31 21:46:15 +0100

    Add Float

--- Variables/float.c
+++ Variables/float.c
@@ -3,9 +3,12 @@
 int main(){
 
   //float
-  float height = 1.75;
+  //float height = 1.75;
+  float weight = 13.5;
 
-  printf("I'm %f tall\n", height);
+
+ // printf("I'm %f tall\n", height);
+  printf("You weigh %f of weight\n", weight);
 
   return 0;
 }
