265f088bcf241e8d3fc079aaec75f7626e098068
buildwithanas <anasamoo0704@gmail.com>
2026-08-02 23:24:49 +0100

    Add Variables

--- Variables/boolean.c
+++ Variables/boolean.c
@@ -4,8 +4,10 @@ int main() {
   //BOOLEAN
 
   bool isonline = true;
+  bool isoffline = false;
 
   printf("Is the user online? %d\n", isonline);
-  
+  printf("Is the user offline? %d\n", isoffline);
+
   return 0;
 }
