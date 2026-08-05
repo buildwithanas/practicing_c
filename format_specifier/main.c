e6cb94ba2748422e8056af30523118a79660bdd9
buildwithanas <anasamoo0704@gmail.com>
2026-08-05 23:51:42 +0100

    Add format_specifier

---
+++ format_specifier/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int age = 17;
+  float price = 35.65;
+  double pi = 4.477482384885885954;
+  char currency = '$';
+  char name[] = "BuildWithAnas";
+
+  printf("%d\n", age);
+  printf("%f\n", price);
+  printf("%lf\n", pi);
+  printf("%c\n", currency);
+  printf("%s\n", name);
+
+
+  return 0;
+}
