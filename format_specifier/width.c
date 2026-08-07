b39f0f15dcddb6c1dea05aa89f739a45b9c84fed
buildwithanas <anasamoo0704@gmail.com>
2026-08-07 23:17:47 +0100

    format_specifer

---
+++ format_specifier/width.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+//HOW TO SET A WIDTH IN C USING FORMAT SPECIFIER
+  int num1 = 1;
+  int num2 = 10;
+  int num3 = 100;
+
+  printf("%3d\n", num1);
+  printf("%3d\n", num2);
+  printf("%3d\n", num3);
+  
+}
