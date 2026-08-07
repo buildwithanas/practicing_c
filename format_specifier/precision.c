89d81ee354db7b667dea742293e37813839de156
buildwithanas <anasamoo0704@gmail.com>
2026-08-07 23:30:15 +0100

    Add Format Specifier

---
+++ format_specifier/precision.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+//HOW TO SET A PRECISION IN C USING FORMAT SPECIFIER
+
+float price1 = 20.96;
+float price2 = 14.65;
+float price3 = -8.03;
+
+printf("%+8.2f\n", price1);
+printf("%+8.2f\n", price2);
+printf("%+8.2f\n", price3);
+
+return 0;
+
+}
