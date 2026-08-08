34abcb133e02d6d5f510c5539942f10801d90c1e
buildwithanas <anasamoo0704@gmail.com>
2026-08-08 23:54:03 +0100

    Add Format Specifier

--- format_specifier/width.c
+++ format_specifier/width.c
@@ -7,10 +7,12 @@ int main(){
   int num1 = 1;
   int num2 = 10;
   int num3 = 100;
+  int num4 = 1000;
 
   printf("%3d\n", num1);
   printf("%3d\n", num2);
   printf("%3d\n", num3);
+  printf("%3d\n", num4);
   
   return 0;
 }
