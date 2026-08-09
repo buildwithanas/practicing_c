2267968400649f38b1ba0857f05cbd572e32bb4c
buildwithanas <anasamoo0704@gmail.com>
2026-08-09 01:52:48 +0100

    Add Arithmetic Operators

---
+++ arithmetic operators/01_arithmetic_operators.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+int main(){
+
+  // Arithmeic operators 
+
+  // ADDITON, SUBTRACTION, MULTIPLICATION, DIVISION USING INTEGERS
+  //int x = 2;
+  //int y = 5;
+  //int z = 10;
+
+  //z = x + y;
+  //y = z - x;
+  //z = y * x;
+  //x = z / y;
+
+  //printf("%d\n", x);
+
+
+
+  // DIVIDING FLOATS USING ARITHMETIC OPERATORS
+  //float p = 8;
+  //float q = 4;
+  //float r = 20;
+
+  //p = q / r;
+
+  //printf("%.2f\n", p);
+
+
+
+// MODULUS OPERATOR
+  //int x = 2;
+  //int y = 5;
+  //int z = 10;
+
+  //x = z % y;
+
+  //printf("%d", x);
+  
+
+  // INCREMENT OPERATOR
+
+  int x = 3;
+  int y = 6;
+  int z = 10;
+
+  //x+=3;
+  //x-=6;
+
+  //x = y * 2;
+  x = y / 2;
+
+  printf("%d\n", x);
+
+  return 0;
+
+}
---
+++ arithmetic operators/05_increment_operator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+  // DECREMENT OPERATOR
+
+  int x = 3;
+  int y = 6;
+  int z = 10;
+
+  x-=3;
+  x = y / 2;
+  z = y - 3
+
+  printf("%d\n", x);
+
+  return 0;
+}
