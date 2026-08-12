4237f1eccf42cce4c27db4b6e6e1f0ffee43a627
buildwithanas <anasamoo0704@gmail.com>
2026-08-12 22:46:26 +0100

    Add Array in C programming

--- Variables/array.c
+++ Variables/array.c
@@ -6,10 +6,12 @@ int main() {
   char name[] = "Anas";
   char foood[] = "Pizza";
   char email[] = "anasamoo0704@gmail.com";
+  char hobby[] = "love Coding";
 
   printf("Hello, my name is %s\n", name);
   printf("I love eating %s\n", foood);
   printf("You can contact me at %s\n", email);
+  printf("My hobby is %s\n", hobby);
 
   return 0;
 }
