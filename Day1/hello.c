24f0a4fdd22382dfc20a93ebd7f006ec0f0dfa87
buildwithanas <anasamoo0704@gmail.com>
2026-08-16 23:40:02 +0100

    Add First Project

--- Day1/hello.c
+++ Day1/hello.c
@@ -2,6 +2,6 @@
 
 int main(){
   printf("Hello World! This is my first C project\n");
-  printf("My aim is to become a great Software");
+  printf("My aim is to become a great Software Engineer");
     return 0;
 }
